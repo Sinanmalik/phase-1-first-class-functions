e5ecfc48d92c888a421f5838eba3db3ad8b08d2a
Sinanmalik <sinanmalik3113@gmail.com>
2024-02-08 11:02:09 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,22 @@
+// index.js
+
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    function namedFunction() {
+    }
+    return namedFunction; 
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+    };
+}
+
+module.exports = {
+    receivesAFunction,
+    returnsANamedFunction,
+    returnsAnAnonymousFunction
+};
